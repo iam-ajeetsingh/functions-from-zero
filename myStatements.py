2581eb87f8654bc0c594b46c7f11e7938671d9b8
Ajeet Singh <ajeet@arizona.edu>
2025-03-01 02:29:44 +0000

    add files to repo

---
+++ myStatements.py
@@ -0,0 +1,11 @@
+my_list = ["apple", "cherry", "lemon"]
+my_dict = {"drink" :"coffee", "milk" : "whole"}
+
+for item in my_list:
+    print(item)
+
+for key,value in my_dict.items():
+    print(value)
+    print(key)
+
+print(my_dict.items())
